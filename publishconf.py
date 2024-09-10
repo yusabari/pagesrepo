0223fb716037b4559f80883343c42466709e6868
yusabari <yumesaki.kr@gmail.com>
2024-09-10 10:08:48 +0900

    changed url for real one

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://yusabari.github.io/"
+SITEURL = "https://yusabari.com"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
